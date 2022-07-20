617a0907345f6a01aff516f89beaa3a77cea9661
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-07-20 18:24:47 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryListComponent
   ],
   imports: [
     BrowserModule,
